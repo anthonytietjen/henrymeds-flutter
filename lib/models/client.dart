b4ae559ce11f13a2e399dc068054eabb70e8daf8
Anthony Tietjen <tietjen.anthony@gmail.com>
2024-02-10 20:30:03 -0700

    Added Client model

---
+++ lib/models/client.dart
@@ -0,0 +1,11 @@
+class Client {
+  final String id;
+  final String firstName;
+  final String lastName;
+
+  Client({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+  });
+}
