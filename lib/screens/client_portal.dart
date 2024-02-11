9c9eded66e2ec760031274973e725f1484375c67
Anthony Tietjen <tietjen.anthony@gmail.com>
2024-02-10 17:29:03 -0700

    Code cleanup

--- lib/screens/client_portal.dart
+++ lib/screens/client_portal.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:henry_meds/components/custom_button.dart';
-import 'package:henry_meds/services/api.dart';
-import 'package:henry_meds/utils/date_utils.dart';
+import 'package:henry_meds/screens/provider_list.dart';
 
 class ClientPortal extends StatefulWidget {
   const ClientPortal({super.key});
@@ -12,7 +11,8 @@ class ClientPortal extends StatefulWidget {
 
 class _ClientPortalState extends State<ClientPortal> {
   void _onScheduleNewAppointmentPressed() async {
-    // TODO: Show next screen
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const ProviderList()));
   }
 
   void _onExistingAppointmentsPressed() {
@@ -30,55 +30,12 @@ class _ClientPortalState extends State<ClientPortal> {
     );
   }
 
-  Future<DateTime?> _promptForDate() async {
-    final firstDate = DateTime.now();
-    final lastDate = DateTime.now().add(const Duration(days: 14));
-    final date = await showDatePicker(
-      context: context,
-      firstDate: firstDate,
-      lastDate: lastDate,
-    );
-    return date;
-  }
-
-  Future<TimeOfDay?> _promptForTime(TimeOfDay initialTime) async {
-    // TODO: Build a better widget that only presents 15 minute increments for selection
-    // TODO: when returning to this after showing message about 15 minute increment requirement, default to the minute selection
-    final time = await showTimePicker(
-      context: context,
-      initialTime: initialTime,
-    );
-
-    if (time != null) {
-      // If it is not a 15 minute increment, prompt again and show a message
-      if (time.minute % 15 != 0) {
-        _promptForTime(time);
-        showDialog(
-          context: context,
-          builder: (BuildContext context) => AlertDialog(
-            title: const Text('Please select 15 minute increment'),
-            actions: [
-              TextButton(
-                child: const Text("OK"),
-                onPressed: () => Navigator.pop(context, 'OK'),
-              ),
-            ],
-          ),
-        );
-      } else {
-        return time;
-      }
-    }
-
-    return null;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Provider Portal'),
+        title: const Text('Client Portal'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
