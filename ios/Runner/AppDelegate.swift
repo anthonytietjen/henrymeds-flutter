6622cd9f1085bf958e6a810f937f7e31b2461a58
Anthony Tietjen <tietjen.anthony@gmail.com>
2024-02-10 19:41:44 -0700

    Added launch screen image

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,8 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    // Delay hiding launch screen for 500 milliseconds
+    usleep(500_000);
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
